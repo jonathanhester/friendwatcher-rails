1457faeec2cea3b2d67fee7aa24f64271b4e587b
Jonathan Hester <hester.jonathan@gmail.com>
2012-11-11 11:11:48 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FriendWatcherRails::Application.config.secret_token = 'e7939737b475559cb9b7c0b083a0b6ff31d674402885927719938f766e2dd2b32463c14c4ab9c00283ea84a46099f487b16ab649ec2581720e6fd7ab73869d76'
