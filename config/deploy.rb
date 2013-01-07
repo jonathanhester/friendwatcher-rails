27ab8e2031b6e57e218c2548e954cc85b3293092
Jonathan Hester <hester.jonathan@gmail.com>
2013-01-06 22:59:50 -0800

    capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,25 +11,21 @@ role :web, "jonathanhester.com"                          # Your HTTP server, Apa
 role :app, "jonathanhester.com"                          # This may be the same as your `Web` server
 role :db,  "jonathanhester.com", :primary => true # This is where Rails migrations will run
 
-set :rvm_ruby_string, 'ruby-1.9.2-p318@friendwatcher'
+set :rvm_ruby_string, 'ruby-1.9.3-p327@friendwatcher'
 require "rvm/capistrano"
 
 set :rvm_type, :system  # Copy the exact line. I really mean :system here
 
-before 'deploy:setup', 'rvm:install_rvm'
+namespace :rvm do
+  task :trust_rvmrc do
+    run "rvm rvmrc trust #{release_path}"
+  end
+end
 
-before 'deploy:setup', 'rvm:install_ruby'
+after "deploy", "rvm:trust_rvmrc"
 
-set :rvm_install_ruby_params, '--with-opt-dir=/usr/local/rvm/usr'
-before 'deploy:setup', 'rvm:install_pkgs'
-
-before 'deploy:setup', 'rvm:import_gemset'
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+before "deploy:update_code", "toldo:compress_assets"
+after "deploy:symlink", "toldo:upload_assets"
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
@@ -37,5 +33,63 @@ namespace :deploy do
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+namespace :deploy do
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+
+    end
+  end
+end
+
+
+namespace :toldo do
+  desc "Compress assets in a local file"
+  task :compress_assets do
+    run_locally("rm -rf public/assets/*")
+    run_locally("bundle exec rake assets:precompile")
+    run_locally("touch assets.tgz && rm assets.tgz")
+    run_locally("tar zcvf assets.tgz public/assets/")
+    run_locally("mv assets.tgz public/assets/")
+  end
+
+  desc "Upload assets"
+  task :upload_assets do
+    upload("public/assets/assets.tgz", release_path + '/assets.tgz')
+    run "cd #{release_path}; tar zxvfm assets.tgz; rm assets.tgz"
+  end
+end
+
+require "delayed/recipes"
+
+set :rails_env, "production" #added for delayed job
+
+after "deploy:stop",    "delayed_job:stop"
+after "deploy:start",   "delayed_job:start"
+after "deploy:restart", "delayed_job:restart"
+
+
+namespace :deploy do
+  desc "Start the Thin processes"
+  task :start do
+    run  <<-CMD
+      /etc/init.d/thin start
+    CMD
+  end
+
+  desc "Stop the Thin processes"
+  task :stop do
+    run <<-CMD
+      /etc/init.d/thin stop
+    CMD
+  end
+
+  desc "Restart the Thin processes"
+  task :restart do
+    run <<-CMD
+      /etc/init.d/thin restart
+    CMD
   end
 end
