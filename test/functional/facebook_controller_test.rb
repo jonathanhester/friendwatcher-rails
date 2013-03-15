777044d8feb59a233497fbdeeb9f801a9a7ad929
Jonathan Hester <hester.jonathan@gmail.com>
2013-03-14 22:25:25 -0700

    make all unit tests work

--- test/functional/facebook_controller_test.rb
+++ test/functional/facebook_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class FacebookControllerTest < ActionController::TestCase
   test "should get rtu" do
-    get :rtu
+    get :rtu, { "hub.mode" => 1 }
     assert_response :success
   end
 
